4d29298bcdc5a8c8e49dd866f8fa031bea9bc40e
1llusory's Dell <1723471165@qq.com>
2024-07-04 12:41:04 +0800

    deploy

--- src/main/java/com/sunhy/config/CorsConfig.java
+++ src/main/java/com/sunhy/config/CorsConfig.java
@@ -10,8 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 允许的前端域名
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的请求方法
+                .allowedOriginPatterns("*") // 允许的前端域名
+                .allowedMethods("*") // 允许的请求方法
                 .allowedHeaders("*") // 允许的请求头
                 .allowCredentials(true); // 允许携带认证信息（例如：cookie）
     }
